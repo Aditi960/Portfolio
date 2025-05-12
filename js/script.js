abed770fc32f35de093d2bbdcb9600ca3d94474f
Aditi960 <aditithakare960@gmail.com>
2025-05-12 20:00:26 +0530

    about section 1.3

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
-
-var typed = new Typed(".typing", {
-    strings: ["","Web Designer", "Web Developer", "Graphic Designer", "YouTuber"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
